3463c86eccb7cfa20ee5b107febd9e2b4fe1def1
lsh <koelkorea@gmail.com>
2022-09-23 15:14:57 +0900

    2nd commit
    변수, 연산자 부분 정리

--- 02. 변수(let, var)와 hoisting개념/variable.js
+++ 02. 변수(let, var)와 hoisting개념/variable.js
@@ -1,113 +1,176 @@
 // 1. Use strict
-// added in ES 5
-// use this for Vanila Javascript.
+//  : added in ES 5, use this for Vanila Javascript.
+//    (ES5에 추가된 개념으로, 쓰면 ECMA가 아니라 순수 바닐라 버전 JS문법을 기준으로 엔진이 JS를 해석)
 'use strict';
 
-// 2. Variable, rw(read/write)
-// let (added in ES6)
-let globalName = 'global name';
+// 2. Variable(변수), rw(read/write)
+//  : 값을 넣으면 읽고, 쓰기 가능한 녀석을 의미
+
+// - let (added in ES6)
+//  : ES6에 추가된 변수로, '블록 스코프'를 기반으로 함. 
+//    -> (참고) 
+//        1) var는 '함수 스코프'를 기반으로 하며, 이 부분으로 인해 hoisting 이슈가 발생
+//        2) 선언 이전에 사용을 할수 없다는 원칙에 대한 개념이 타 언어의 변수개념과 비슷하여, TDZ개념 존재
+//           (TDZ : Temperary Dead Zone의 약자로, 해당 스코프에서 변수 선언 위치 이전 영역을 의미함)
+
+// # block scope (블록 스코프)
+//  : 일종의 변수들이 통용되는 영역 구분을 위한 '결계'와 같은 것으로 { }로 영역을 설정. (중요) 최상위는 global scope!
+//    -> 이 결계는 '일종의 특수거울'과 같이 '안에서는 밖을 볼 수 있고, 밖에서는 안을 볼수 없음
+//       -> 이 결계를 기준으로 밖에서 선언된 변수는 일종의 전역변수, 안에서 선언된 변수는 지역변수의 논리가 통용
+
+// -------------------------------------------------------------------------------------------------------
+// (ex) 블록 스코프 사용 예시
+let globalName = 'global name'; // 블록 스코프 밖 전역변수(혹은 global scope에 선언된 변수) globalName
 {
-  let name = 'ellie';
-  console.log(name);
+  let name = 'ellie'; // 블록 스코프 내부의 지역변수 name
+  console.log(name);  // ellie
+
   name = 'hello';
-  console.log(name);
-  console.log(globalName);
+  console.log(name);  // hello
+
+  // (중요) 블록 스코프 내부에서는 밖에서 선언된 변역변수의 값에 접근이 가능 (특수거울 기억하자)
+  console.log(globalName);  // global name 
 }
+
+// (중요) 해당 위치 이전을 기준으로 특정 블록 스코프 안에서만 선언된 변수는 일종의 지역변수로서 보기에, 스코프 밖에서는 접근이 불가능함
 console.log(name);
 console.log(globalName);
+//---------------------------------------------------------------------------------------------------------
 
-// var (don't ever use this!)
-// var hoisting (move declaration from bottom to top)
-// has no block scope
+// - var (don't ever use this!)
+//  : ES6 이전에 쓰이던 변수, '함수 스코프' 를 기반으로 하는데, 타언어의 변수개념과 동떨어져 있기에 안 쓰는걸 추천
+
+// # function scope (함수 스코프)
+//  : 블록 스코프의 결계 개념이 function의 경우에만 통용되는 개념
+//    -> 함수 스코프 기준으로 지역변수 개념? function의 { }내에서만 통용됨
+//       (= var의 호이스팅이 의도치 않게 문제를 만드는 이유)
+
+// # hoisting (호이스팅) (move declaration from bottom to top)
+//  : var나 function의 경우 아래에서 선언한 것들이 실행단계에서 최상단에 올라와서 일괄적으로 선언되는 현상
+//    -> (중요) why var의 경우는 문제가 되는가 () ?
+//        : BUT! 함수 스코프가 기반이 되는 var의 경우, 호이스팅 발생시, 조건문이나 반복문의 스코프까지 깡그리 선언 순서를 무시함
+//          (= 타 언어의 변수 개념을 생각했다가는 기습당하기 딱 좋음.. 특히 for문이 그럼)
+// 
+//        <-> (중요) let도 호이스팅이 발생하나, 블록스코프 개념을 TDZ설정으로 선언 위치와 사용 위치 관계 정립하여 해결
+
+// -------------------------------------------------------------------------------------------------------
+// (ex) var hoisting의 황당함 예시
 {
-  age = 4;
-  var age;
+  age = 4;  // 변수 사용이
+  var age;  // 선언 시점보다 빠르다...
 }
-console.log(age);
+console.log(age); // 4  <- 근데 문제없이 인식했다?! 심지어 블록 스코프 안인데?
 
-// 3. Constant, r(read only)
-// use const whenever possible.
-// only use let if variable needs to change.
+// ??? : 블록 스코프 결계? 그거 먹는거임? 날 잡고 싶으면 function 결계만 치던가~~~ ㅋㅋㅋ
+//---------------------------------------------------------------------------------------------------------
+
+
+// 3. Constant(상수), r(read only)
+//  : use const whenever possible (only use let, if variable needs to change)
+//    (값을 일단 한번 넣으면, 변경X 읽기만 가능한 녀석을 의미)
 const daysInWeek = 7;
 const maxNumber = 5;
 
-// Note!
-// Immutable data types: premitive types, frozen objects (i.e. object.freeze())
-// Mutable data types: all objects by default are mutable in JS
-// favor immutable data type always for a few reasons:
-//  - security
-//  - thread safety
-//  - reduce human mistakes
+// # 변경 불가능한 자료형 : premitive types(원시형), frozen objects (i.e. object.freeze())
+// # 변경 가능한 자료형 : all objects by default are mutable in JS (객체의 매개변수들은 모두 변이 가능)
 
-// 4. Variable types
-// primitive, single item: number, string, boolean, null, undefined, symbol
-// object, box container
-// function, first-class function
+// # favor immutable data type always for a few reasons 
+//   (변경이 불가능한 상수를 쓰면 좋은 이유)
+//  - security (해킹 관련 보안)
+//  - thread safety (멀티쓰레드의 경우, 동시다발적 접근으로 인해 프로그램이 개판되는 상황을 방지 가능)
+//  - reduce human mistakes (사람의 실수 가능성을 원천적으로 줄일 수 있음)
 
-// number
+// 4. Variable types (변수 자료형)
+//  - primitive (원시형) : number, string, boolean, null, undefined, symbol 과 같은 단일 변수
+//  - object(객체)
+//  - box container
+//  - function(함수) : JS는 first-class function (1급 힘수)가 지원되는 언어 (= 함수를 변수, return문, 함수의 인자로 할당 가능한 언어 의미)
+
+//  - primitive (원시형)
+
+// a) number
+//  : integer(정수), decimal number(소수) 던 상관없이 하나로 통일
 const count = 17; // integer
 const size = 17.1; // decimal number
+
 console.log(`value: ${count}, type: ${typeof count}`);
 console.log(`value: ${size}, type: ${typeof size}`);
 
-// number - speicla numeric values: infinity, -infinity, NaN
-const infinity = 1 / 0;
+// # 특수한 숫자값 유형
+//   : infinity (양의 무한), -infinity (음의 무한), NaN (Not A Number : 숫자가 아닌데, 숫자로 출력해야 하는경우 나오는 값)
+const infinity = 1 / 0; 
 const negativeInfinity = -1 / 0;
 const nAn = 'not a number' / 2;
+
 console.log(infinity);
 console.log(negativeInfinity);
 console.log(nAn);
 
-// bigInt (fairly new, don't use it yet)
+// # bigInt (fairly new, don't use it yet)
+//  : Java의 bigInt와 같이 기존 소수값 변수(자바의 경우 double)보다 더 큰 값을 표현가능한 변수타입
 const bigInt = 1234567890123456789012345678901234567890n; // over (-2**53) ~ 2*53)
 console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
 
-// string
+// b) string
+//  : 문자에 대한 자료형으로 문자열의 경우는 배열개념으로 조합되어 있기에, 배열을 통해 원하는 위치의 문자를 찾는것이 가능
 const char = 'c';
 const brendan = 'brendan';
 const greeting = 'hello ' + brendan;
+const helloBob = `hi ${brendan}!`; //template literals (string) : `(백틱) 부호를 이용해서 문자열 표현이 쉽게 가능함 참고
+
 console.log(`value: ${greeting}, type: ${typeof greeting}`);
-const helloBob = `hi ${brendan}!`; //template literals (string)
 console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
 console.log('value: ' + helloBob + ' type: ' + typeof helloBob);
 
-// boolean
-// false: 0, null, undefined, NaN, ''
-// true: any other value
+// c) boolean : 참/거짓... 
+//   -> (참고) 0, null, undefined, NaN, '' 에 해당하는 값들은 false
 const canRead = true;
 const test = 3 < 1; // false
 console.log(`value: ${canRead}, type: ${typeof canRead}`);
 console.log(`value: ${test}, type: ${typeof test}`);
 
-// null
+// d) null : null이라고 사용자가 선언한 경우에 할당되는 자료형 (undefined는 사용자는 개입한거 없는데 비어있을때 등장)
 let nothing = null;
 console.log(`value: ${nothing}, type: ${typeof nothing}`);
 
-// undefined
+// e) undefined : 값이 그냥 비었을경우 (null은 사용자가 null이라고 명시)
 let x;
 console.log(`value: ${x}, type: ${typeof x}`);
 
-// symbol, create unique identifiers for objects
+// f) symbol
+//   : create unique identifiers for objects
+//     (특수한 경우 고유한 특별자를 만들때 사용)
+
+// # Symbol('문자열') : 해당 명령으로 생성한 식별자는 문자열과 상관없이 다른 내용을 지닌 객체로 취급
 const symbol1 = Symbol('id');
 const symbol2 = Symbol('id');
 console.log(symbol1 === symbol2);
+
+// # Symbol.for('문자열') : 해당 명령으로 생성한 식별자는 문자열이 같으면 같은 객체로 취급
 const gSymbol1 = Symbol.for('id');
 const gSymbol2 = Symbol.for('id');
 console.log(gSymbol1 === gSymbol2); // true
+
 console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
 
-// object, real-life object, data structure
-const ellie = { name: 'ellie', age: 20 };
-ellie.age = 21;
+// g) object (객체)
+//  : real-life object, data structure (일상생활의 대상을 설명하는 데이터 구조... <-> JSon)
+//    -> 단! 객체는 너무 커서 한번에 메모리에 할당하지 않고, 수정 불가능함 객체명을 ref값으로 그 주소를 적어놓는 식으로 객체를 구성하는 데이터들의 위치를 담음
+const ellie = { name: 'ellie', age: 20 }; //  여기서 객체명 ellie는 객체를 구성하는 다른 데이터들이 담긴 메모리의 위치값을 담고 있음
+ellie.age = 21; // 그래서 객체명을 통해 객체의 맴버변수에 접근 및 내용 변경이 가능함.
 
-// 5. Dynamic typing: dynamically typed language
-let text = 'hello';
+// 5. Dynamic typing (동적 타이핑)
+//  : JS는 동적 타입이 지원되는 언어라는 것에 대한 설명 (대충 타입스크립트가 나온 배경 설명)
+let text = 'hello'; // 처음에 문자열이었지만
 console.log(text.charAt(0)); //h
 console.log(`value: ${text}, type: ${typeof text}`);
+
 text = 1;
 console.log(`value: ${text}, type: ${typeof text}`);
-text = '7' + 5;
+
+text = '7' + 5; //  문자숫자 + 숫자 -> 문자열 연결로 인정
 console.log(`value: ${text}, type: ${typeof text}`);
-text = '8' / '2';
-console.log(`value: ${text}, type: ${typeof text}`);
-console.log(text.charAt(0));
+
+text = '8' / '2'; // 문자숫자 / 문자숫자? -> 숫자로 인정
+console.log(`value: ${text}, type: ${typeof text}`);  // 이렇게 숫자로 변경 가능
+console.log(text.charAt(0));  // 단! 메서드는 그 타입을 가리지 못해서 에러를 낼수 있음... (처음부터 타입을 가려받자! = TS존재 의미)
--- 03. 연산자(operator),  조건문(if), 반복문(for)/operator.js
+++ 03. 연산자(operator),  조건문(if), 반복문(for)/operator.js
@@ -1,7 +1,13 @@
-// 1. String concatenation
-console.log('my' + ' cat');
-console.log('1' + 2);
-console.log(`string literals: 1 + 2 = ${1 + 2}`);
+// 1. String concatenation (문자열 합치기)
+//  - 문자열 + 문자열
+//  - 문자열 + 숫자 -> 문자숫자로 변환해서 연결해줌 
+//  - `(백틱) 안에서 ${ } 안에 변수나 객체의 맴버변수등을 사용해서, 해당 내용을 문자열에 포함시킬 수 있다
+console.log('my' + ' cat'); // my cat
+console.log('1' + 2); // '12'
+console.log(`string literals: 1 + 2 = ${1 + 2}`); //string literals: 1 + 2 = 3
+
+//  # ''로 선언한 문자열에 특수기호를 넣고 싶다면?
+//    : \n (줄바꾸기), \' (쉼표) 등 \를 사용해서 알아보면 됨
 
 // 2. Numeric operators
 console.log(1 + 1); // add
@@ -11,22 +17,29 @@ console.log(1 * 1); // multiply
 console.log(5 % 2); // remainder
 console.log(2 ** 3); // exponentiation
 
-// 3. Increment and decrement operators
+// 3. Increment and decrement operators (증가, 증감 연산자)
+//  : ++, --을 사용하는 연산자로 
+//    -> (중요)
+//        - (ex ++변수명) 연산자가 앞에 있으면, 그 자리에서 1을 바로 증감 후 변수에 업뎃해 반영
+//        - (ex 변수명++) 연산자가 뒤에 있으면, 당장은 1을 증감하지 않고, 해당 연산자와 연관된 표기가 끝난 이후 변수를 업뎃해 반영
 let counter = 2;
 const preIncrement = ++counter;
 // counter = counter + 1;
 // preIncrement = counter;
 console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+
 const postIncrement = counter++;
 // postIncrement = counter;
 // counter = counter + 1;
 console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
+
 const preDecrement = --counter;
 console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
+
 const postDecrement = counter--;
 console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
 
-// 4. Assignment operators
+// 4. Assignment operators (할당 연산자)
 let x = 3;
 let y = 6;
 x += y; // x = x + y;
@@ -34,24 +47,32 @@ x -= y;
 x *= y;
 x /= y;
 
-// 5. Comparison operators
+// 5. Comparison operators (비교 연산자)
 console.log(10 < 6); // less than
 console.log(10 <= 6); // less than or equal
 console.log(10 > 6); // greater than
 console.log(10 >= 6); // greater than or equal
 
-// 6. Logical operators: || (or), && (and), ! (not)
+// 6. Logical operators (논리 연산자)
+//  : || (or), && (and), ! (not)
+
+// # often used to compress long if-statement (X중 조건문의 실행을 효율적으로 하기 위해 || &&를 사용하기도 한다)
+// # nullableObject && nullableObject.something (객체에 null값이 있는 경우 여부의 빠른 판별을 위해서, X중 &&구문을 사용하기도 한다)
+
 const value1 = true;
 const value2 = 4 < 2;
 
-// || (or), finds the first truthy value
+// - || (or)
+//  : finds the first truthy value 
+//    (x중 or구문은 하나라도 참이면 성립하니, 엔진은 순차적으로 하나라도 참이 나오면, 뒤는 더 보지 않기에)
+//      -> 연산이 무거운 건 논리변수 및 함수는 가급적 뒤에다 두는게 효율적임
 console.log(`or: ${value1 || value2 || check()}`);
 
-// && (and), finds the first falsy value
+// - && (and)
+//  : finds the first falsy value
+//    (x중 and구문은 하나라도 거짓이면 성립하니, 엔진은 순차적으로 하나라도 거짓이 나오면, 뒤는 더 보지 않는다)
+//      -> 연산이 무거운 건 논리변수 및 함수는 가급적 뒤에다 두는게 효율적임
 console.log(`and: ${value1 && value2 && check()}`);
-
-// often used to compress long if-statement
-// nullableObject && nullableObject.something
 
 function check() {
   for (let i = 0; i < 10; i++) {
@@ -61,38 +82,46 @@ function check() {
   return true;
 }
 
-// ! (not)
+// - ! (not)
 console.log(!value1);
 
-// 7. Equality
+// 7. Equality (동등 연산자 == , 일치 연산자 ===)
+//  : 피연산자들의 내용 일치 여부를 판단하는 연산자
+//    -> (중요) 둘의 차이는 일치여부 기준이 단순 내용만 같은지 보냐(==)? 아니면 자료형까지 같아야 하나(===)? 에 있다
 const stringFive = '5';
 const numberFive = 5;
 
-// == loose equality, with type conversion
+// - 동등 연산자 == 
+//  : loose equality, with type conversion
+//    (자료형이 변경되어도 내용이 같기만 하면 같다고 느슨한 판정)
 console.log(stringFive == numberFive);
 console.log(stringFive != numberFive);
 
-// === strict equality, no type conversion
+// - 일치 연산자 === 
+//  : strict equality, no type conversion
+//    (자료형까지 일치여부를 확인하여, 일치여부 확인하는 엄격한 판정)
 console.log(stringFive === numberFive);
 console.log(stringFive !== numberFive);
 
-// object equality by reference
+// # object equality by reference
 const ellie1 = { name: 'ellie' };
 const ellie2 = { name: 'ellie' };
 const ellie3 = ellie1;
-console.log(ellie1 == ellie2);
-console.log(ellie1 === ellie2);
-console.log(ellie1 === ellie3);
 
-// equality - puzzler
-console.log(0 == false);
-console.log(0 === false);
-console.log('' == false);
-console.log('' === false);
-console.log(null == undefined);
-console.log(null === undefined);
+console.log(ellie1 == ellie2);  // false : 참고하는 ref가 다르기에, 참고하는 객체도 다르기에, 당연히 내용도 다름
+console.log(ellie1 === ellie2); // false : 이미 객체의 내용도 다르지만, 자료형까지 ellie1과 ellie2로 각각 다름
+console.log(ellie1 === ellie3); // true : 자료형이 ellie1을 보기까지 같음
 
-// 8. Conditional operators: if
+// false에 속하는 값들 equality 예시
+console.log(0 == false);          // true
+console.log(0 === false);         // false : 0은 number =/ false는 boolean
+console.log('' == false);         // true
+console.log('' === false);        // false : ''은 string =/ false는 boolean
+console.log(null == undefined);   // true
+console.log(null === undefined);  // false : null과 undefined는 다른 자료형
+
+// 8. Conditional operators(조건 연산자) : if
+
 // if, else if, else
 const name = 'df';
 if (name === 'ellie') {
@@ -103,11 +132,13 @@ if (name === 'ellie') {
   console.log('unkwnon');
 }
 
-// 9. Ternary operator: ?
+// 9. Ternary operator(삼항 연산자) : ?
+
 // condition ? value1 : value2;
 console.log(name === 'ellie' ? 'yes' : 'no');
 
-// 10. Switch statement
+// 10. Switch statement(스위치 구문)
+
 // use for multiple if checks
 // use for enum-like value check
 // use for multiple type checks in TS
@@ -125,23 +156,27 @@ switch (browser) {
     break;
 }
 
-// 11. Loops
-// while loop, while the condition is truthy,
-// body code is executed.
+// 11. Loops (반복문)
+
+// - while loop
+//   : while the condition is truthy, body code is executed
+//     (내용이 조건에 true값이 나올때까지 반복함)
 let i = 3;
 while (i > 0) {
   console.log(`while: ${i}`);
   i--;
 }
 
-// do while loop, body code is executed first,
-// then check the condition.
+// - do while loop
+//    : body code is executed first, then check the condition.
+//      (while과 같지만, do안의 내용을 먼저 시작하고, while구문 진행)
 do {
   console.log(`do while: ${i}`);
   i--;
 } while (i > 0);
 
-// for loop, for(begin; condition; step)
+// - for loop
+//  : for(begin; condition; step)
 for (i = 3; i > 0; i--) {
   console.log(`for: ${i}`);
 }
@@ -151,14 +186,18 @@ for (let i = 3; i > 0; i = i - 2) {
   console.log(`inline variable for: ${i}`);
 }
 
-// nested loops
+// # nested loops (이중 for문)
 for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
     console.log(`i: ${i}, j:${j}`);
   }
 }
 
-// break, continue
+// # break, continue
+//  : 일종의 return 역할을 하는 구문으로
+//    -> break : 그 자리에서 반복문 멈춤
+//    -> continue : 현재의 싸이클을 skip하고, 다음 반복 싸이클로 넘어가서 반복문을 계속 수행
+
 // Q1. iterate from 0 to 10 and print only even numbers (use continue)
 for (let i = 0; i < 11; i++) {
   if (i % 2 === 0) {
